5aa00a52750a82ce30cf8878bcdd29b61f62af37
afsarakhaa <afsarrakha140604@gmail.com>
2025-06-11 17:57:36 +0700

    feat: Add 2 more projects to Projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,6 +3,16 @@ import type { Project } from "@/types"
 export const projects: Project[] = [
     {
         id: 1,
+        title: "JadiKerja: Job-Seeking Platform",
+        description:
+            "Designed a job-seeking platform to help reduce unemployment in Jakarta. Selected as a Top 10 finalist and representative of South Jakarta in the Ide Menyala Hackathon 2024.",
+        image: "/image/projects/jadikerja.jpeg",
+        technologies: ["Vue.js", "Nest.js", "PostgreSQL", "Tailwind CSS", "Figma"],
+        github: "",
+        demo: "https://jadikerja-fe.vercel.app",
+    },
+    {
+        id: 2,
         title: "SWARA: Legal Support Platform",
         description:
             "Developed in just 36 hours for a hackathon held, SWARA is a comprehensive online platform designed to democratize legal support for all Indonesians, particularly marginalized communities.",
@@ -12,7 +22,17 @@ export const projects: Project[] = [
         demo: "https://swara-web.vercel.app",
     },
     {
-        id: 2,
+        id: 3,
+        title: "ApapMedika - Healthcare System",
+        description:
+            "Built a microservice-based healthcare system consisting of six services: authentication, appointment, inpatient, pharmacy, insurance, and billing.",
+        image: "/image/projects/apapmedika.png",
+        technologies: ["Vue.js", "Spring Boot", "PostgreSQL", "Tailwind CSS", "Figma"],
+        github: "https://gitlab.cs.ui.ac.id/apap-2024/tk-kelompok-32/apap-medika-web",
+        demo: "https://apap-2077.cs.ui.ac.id",
+    },
+    {
+        id: 4,
         title: "Edupeer: Online P2P Tutor UI Design",
         description:
             "EduPeer is a peer-to-peer tutoring platform that connects students with tutors from their own major. This project was a finalist in the GEMASTIK XVI UI/UX competition and addresses key SDGs for education and inequality.",
@@ -22,7 +42,7 @@ export const projects: Project[] = [
         demo: "https://linktr.ee/EduPeer",
     },
     {
-        id: 3,
+        id: 5,
         title: "GeTheBooks: Online Bookstore App",
         description:
             "GetTheBooks is a web and mobile app designed to provide easy access to a book catalog and a shopping cart feature.",
@@ -32,7 +52,7 @@ export const projects: Project[] = [
         demo: "https://gethebooks-c03-tk.pbp.cs.ui.ac.id",
     },
     {
-        id: 4,
+        id: 6,
         title: "Joobie: Job Seeker UI Design",
         description:
             "Joobie is a Jobseeker Application to make it easier for job hunters to find jobs. Joobie is made with the Design thinking method which is how to solve problems from a design point of view which is trying to understand the user.",
@@ -42,7 +62,7 @@ export const projects: Project[] = [
         demo: "https://lnkd.in/gy_K5gTi ",
     },
     {
-        id: 5,
+        id: 7,
         title: "KeliLINK: Public Transport UI Design",
         description:
             "KeliLINK is a mobile application that integrates with various public transportation services such as KRL, Transjakarta, MRT, LRT, and the ride-hailing app Grab.",
@@ -52,7 +72,7 @@ export const projects: Project[] = [
         demo: "https://www.figma.com/proto/p1mlLFrxNcBk8wOUJk1x0i/KeliLINK---Prototype?node-id=68-370&viewport=673%2C475%2C0.05&t=T8qiKBbHJI81Cm4O-0&scaling=scale-down&starting-point-node-id=68%3A370&show-proto-sidebar=1",
     },
     {
-        id: 6,
+        id: 8,
         title: "Attur: Task Management UI Design",
         description:
             "Attur is a task manager application concept designed for the PEKAN RISTEK 2022 UI/UX Design Competition. Focused on creating an intuitive and efficient user experience, this project was awarded the Gold Medal (1st Place) in the competition.",
